80001fd3e1b57fc9b172b0efc8cc3b49a90ffd85
Jason Watson <jason.watson@corp.aol.com>
2010-04-21 21:13:55 -0400

    moving some template files around to clean up the working directory.

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mkdir -p /var/db/logjammin/static
-cp *.json *.html /var/db/logjammin/
-cp *.js *.css /var/db/logjammin/static
-cp build/Debug/logjammin /Library/WebServer/CGI-Executables/logjammin
